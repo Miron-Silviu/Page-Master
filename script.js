3fea4d11405b8cdf716dab66c7ebd730abd0be49
Silviu <silviu_miron_90@outlook.com>
2025-01-21 13:56:14 +0000

    WIP Create funcionality for Notify Me button
    -Create new variables for input and notify me button and litener event.
    - Create functionality for notify me button.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// input variable
+
+const emailInput = document.querySelector('.email-input');
+emailInput.value;
+
+// button variables
+
+const buttonNotfication = document.querySelector('.notification-button');
+
+const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+
+buttonNotfication.addEventListener('click', function () {
+  //   console.log(emailInput.value);
+  emailInput.value === emailPattern;
+  if (emailInput.value === '') {
+    alert(`Whoops! It looks like you forgot to add your email`);
+  } else if (emailInput.value != emailPattern) {
+    alert(`Please provide a valid email address`);
+  } else {
+    alert(`Your email address is good `);
+  }
+  //   TODO SOlVE THE ISSUE WITH  "emailInput.value != emailPattern"
+});
